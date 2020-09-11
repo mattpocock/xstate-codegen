7b8e3ed1fda2731ff5f9ea7402cf1d45f805c79a
Matt Pocock <matt.pocock@yozobi.com>
2020-09-11 18:38:05 +0100

    Added the ability to pass options as a second parameter

--- packages/xstate-compiled/src/__tests__/printToFile.test.ts
+++ packages/xstate-compiled/src/__tests__/printToFile.test.ts
@@ -3,35 +3,61 @@ import { getDeclarationFileTexts } from '../printToFile';
 import { introspectMachine } from '../introspectMachine';
 
 test('That printToFile matches a snapshot test', () => {
-  const machine = Machine({
-    initial: 'invokeTest',
-    id: 'superMachine',
-    on: {},
-    states: {
-      invokeTest: {
-        invoke: {
-          src: 'someInvoke',
-        },
-        onDone: 'testEvent',
-      },
-      testEvent: {
-        on: {
-          SOMETHING: 'activityTest',
-        },
-      },
-      activityTest: {
-        activities: ['someActivity'],
-        on: {
-          CONDITION_TEST: [
+  const machine = Machine(
+    {
+      initial: 'invokeTest',
+      id: 'superMachine',
+      on: {},
+      states: {
+        invokeTest: {
+          invoke: [
             {
-              cond: 'condition',
-              target: 'invokeTest',
+              src: 'someInvoke',
+            },
+            {
+              src: 'notRequiredInvoke',
             },
           ],
+          onDone: 'testEvent',
+        },
+        testEvent: {
+          entry: ['action', 'notRequiredAction'],
+          on: {
+            SOMETHING: 'activityTest',
+          },
+        },
+        activityTest: {
+          activities: ['someActivity', 'notRequiredActivity'],
+          on: {
+            CONDITION_TEST: [
+              {
+                cond: 'condition',
+                target: 'invokeTest',
+              },
+              {
+                cond: 'notRequiredCondition',
+                target: 'invokeTest',
+              },
+            ],
+          },
         },
       },
     },
-  });
+    {
+      actions: {
+        notRequiredAction: () => {},
+      },
+      guards: {
+        notRequiredCondition: () => false,
+      },
+      activities: {
+        notRequiredActivity: () => {},
+      },
+      services: {
+        notRequiredInvoke: async () => {},
+      },
+    },
+  );
 
   const files = getDeclarationFileTexts({
     'index.js': { id: 'something', ...introspectMachine(machine) },
