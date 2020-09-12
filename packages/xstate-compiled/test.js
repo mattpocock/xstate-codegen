0b1ecbe61fd4d0006013cce42989ec39bead7007
Matt Pocock <matt.pocock@yozobi.com>
2020-09-12 13:46:47 +0100

    Removed CI dependency on ts-node

--- packages/xstate-compiled/test.js
+++ packages/xstate-compiled/test.js
@@ -1,9 +1,20 @@
 const { execSync } = require('child_process');
 
 // Runs in execSync so that it can easily work with nodemon
+execSync(`cd examples && rm -rf node_modules && npm i --silent`, {
+  stdio: 'inherit',
+});
+
 execSync(
-  `cd examples && rm -rf node_modules && npm i --silent && ts-node -T ../src/index.ts \"*.machine.ts\" --once && npm run build`,
+  `cd examples && ${
+    // If in CI, use node. If in local development, use ts-node
+    process.env.CI ? 'node ../bin/index.js' : 'ts-node -T ../src/index.ts'
+  } "*.machine.ts" --once`,
   {
     stdio: 'inherit',
   },
 );
+
+execSync('cd examples && npm run build', {
+  stdio: 'inherit',
+});
