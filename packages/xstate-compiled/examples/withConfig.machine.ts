d61ef7124ee2c029d48340e6cd0295a8c21c117c
Matt Pocock <matt.pocock@yozobi.com>
2020-09-19 14:56:15 +0100

    Fixed withConfig so that it is strongly typed to the _options attribute

---
+++ packages/xstate-compiled/examples/withConfig.machine.ts
@@ -0,0 +1,50 @@
+import { Machine, interpret } from '@xstate/compiled';
+
+type Data = {
+  yeah: boolean;
+};
+
+interface Context {
+  data: Data;
+}
+
+type Event =
+  | { type: 'MAKE_FETCH'; params: { id: string } }
+  | { type: 'CANCEL' }
+  | { type: 'done.invoke.makeFetch'; data: Data };
+
+const machine = Machine<Context, Event, 'withConfigTest'>({
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        MAKE_FETCH: 'pending',
+      },
+    },
+    pending: {
+      invoke: [
+        {
+          src: 'makeFetch',
+          onDone: 'success',
+        },
+      ],
+    },
+    success: {
+      entry: ['celebrate'],
+    },
+  },
+});
+
+/**
+ * withConfig is a partial of the full options
+ */
+machine.withConfig({
+  actions: {
+    // @ts-expect-error
+    wrongActionName: () => {},
+  },
+  services: {
+    // @ts-expect-error
+    wrongServiceName: () => {},
+  },
+});
